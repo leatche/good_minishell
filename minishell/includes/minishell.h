10d3ec77618f7293c1362f75ba3529de7b9fed86
Lea Tcherepoff <ltcherep@c2r8p5.42nice.fr>
2025-04-08 16:26:41 +0200

    premier push

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltcherep <ltcherep@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 15:01:44 by ltcherep          #+#    #+#             */
+/*   Updated: 2025/04/08 15:07:44 by ltcherep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL.H
+#define MINISHELL.H
+
+//librairies
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <string.h>
+# include <limits.h>
+
+//structure
+typedef struct s_env
+{
+
+}				t_env;
+
+//parsing
+//execution
+
+#endif
