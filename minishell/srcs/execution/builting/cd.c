22dc00115d41b2504c8e55b761225bb225319acd
Krain <thekrainbow@gmail.com>
2025-04-16 18:37:23 +0200

    j

--- minishell/srcs/execution/builting/cd.c
+++ minishell/srcs/execution/builting/cd.c
@@ -23,13 +23,41 @@ void    ft_cd(int ac, char **av) //, char **env
 void    ft_cd_exe(char **av) //, char **env
 {
 
-    chdir(av[2]);
-    // if (res == 0)
-    //     ft_update_path();
-    // else
-    // {
-    //     write(2, "cd: ", 5);
-    //     perror(av[1]);
-    // }
+    if (av[2] == "" || av[2] == '~')
+        ft_cd_empty();
+    else if (av[2] == '-')
+        ft_cd_back();
+    else
+        ft_cd_dir(av[2]);
 }
-       
+
+void    ft_cd_empty()
+{
+    char    *path;
+
+    path = getenv("HOME");
+    chdir(path);
+}
+
+void    ft_cd_back()
+{
+    char    *path;
+
+    path = getenv("OLDPWD");
+    chdir(path);
+    printf("%s\n", path);
+}
+
+void    ft_cd_dir(char *av)
+{
+    char    *previous_path;
+
+    previous_path = getcwd(NULL, 0);
+    $OLDPWD = previous_path; // faire deux fonctions
+    if (chdir(av) != 0)
+    {
+        perror("cd");
+        return ;
+    }
+    $PWD = getcwd(NULL, 0); // faire deux fonctions
+}
