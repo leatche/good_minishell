c21a29d2917fa6195748fd3c243c5482a9fe870d
Krain <thekrainbow@gmail.com>
2025-04-17 12:38:15 +0200

    h

--- minishell/srcs/execution/builting/cd.c
+++ minishell/srcs/execution/builting/cd.c
@@ -12,52 +12,65 @@
 
 #include "minishell.h"
 
-void    ft_cd(int ac, char **av) //, char **env
+void    ft_cd(int ac, char **av, char **env)
 {
     if (ac == 3)
-        ft_cd_exe(av);
+        ft_cd_exe(av, env);
     else
         printf("cd: too many arguments\n");
 }
 
-void    ft_cd_exe(char **av) //, char **env
+void    ft_cd_exe(char **av, char **env)
 {
 
     if (av[2] == "" || av[2] == '~')
-        ft_cd_empty();
+        ft_cd_empty(env);
     else if (av[2] == '-')
-        ft_cd_back();
+        ft_cd_back(env);
     else
-        ft_cd_dir(av[2]);
+        ft_cd_dir(av[2], env);
 }
 
-void    ft_cd_empty()
+void    ft_cd_empty(char **env)
 {
     char    *path;
+    char    *actual_path;
 
+    actual_path = getcwd(NULL, 0);
     path = getenv("HOME");
+    ft_old_pwd(actual_path, env);
     chdir(path);
 }
 
-void    ft_cd_back()
+void    ft_cd_back(char **env)
 {
-    char    *path;
+    char    *future_path;
+    char    *actual_path;
 
-    path = getenv("OLDPWD");
-    chdir(path);
-    printf("%s\n", path);
+    actual_path = getcwd(NULL, 0);
+    future_path = getenv("OLDPWD");
+    ft_old_pwd(actual_path, env);
+    chdir(future_path);
+    printf("%s\n", future_path);
 }
 
-void    ft_cd_dir(char *av)
+void    ft_cd_dir(char *av, char **env)
 {
-    char    *previous_path;
+    char    *actual_path;
+    char    *new_path;
 
-    previous_path = getcwd(NULL, 0);
-    $OLDPWD = previous_path; // faire deux fonctions
-    if (chdir(av) != 0)
+    actual_path = getcwd(NULL, 0);
+    ft_old_pwd(actual_path, env);
+    new_path = chdir(av);
+    if (new_path != 0)
     {
         perror("cd");
         return ;
     }
-    $PWD = getcwd(NULL, 0); // faire deux fonctions
 }
+
+void    ft_old_pwd(char *path, char **env)
+{
+
+}
+
